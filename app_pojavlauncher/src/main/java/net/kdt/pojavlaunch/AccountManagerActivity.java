11249f3f876806c990717df21c11201d58302b6f
kakavpopee <kysnow978@gmail.com>
2026-07-06 15:13:04 +0200

    Add Account Manager page

---
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/AccountManagerActivity.java
@@ -0,0 +1,15 @@
+package net.kdt.pojavlaunch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.greecroafto.crystal.R;
+
+public class AccountManagerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_manager);
+    }
+}
