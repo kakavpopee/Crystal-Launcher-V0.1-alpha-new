6359bda182c5628c7ab8d71b478e648c6f43fe90
kakavpopee <kysnow978@gmail.com>
2026-07-06 21:57:29 +0200

    Connect Account Manager to existing login flow

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/AccountManagerActivity.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/AccountManagerActivity.java
@@ -2,8 +2,12 @@ package net.kdt.pojavlaunch;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.greecroafto.crystal.R;
+
+import net.kdt.pojavlaunch.extra.ExtraConstants;
+import net.kdt.pojavlaunch.extra.ExtraCore;
 
 public class AccountManagerActivity extends Activity {
 
@@ -11,5 +15,12 @@ public class AccountManagerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_manager);
+
+        Button addAccount = findViewById(R.id.button_add_account);
+
+        addAccount.setOnClickListener(v -> {
+            ExtraCore.setValue(ExtraConstants.SELECT_AUTH_METHOD, true);
+            finish();
+        });
     }
 }
